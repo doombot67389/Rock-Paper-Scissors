17c3ec88c835d8bd06f93497687eb8bb40e15771
doombot67389 <doombot@warpmail.net>
2022-05-22 17:52:53 -0500

    completed 1st function

--- rps.js
+++ rps.js
@@ -1,7 +1,24 @@
-//psuedocode
-// function; plays against computer
-let a = computerPlay() {
+// function; randomly chooses be between rock, paper, and scissors
+function computerPlay() {
+ let r = Math.floor(Math.random() * 3)
 
+    switch(r) {
+    case 0:
+      console.log('Rock');
+      break;
+    case 1:
+      console.log('Scissors');
+      break;
+    case 2:
+      console.log('Paper');
+      break;
+  }
+}
+
+// plays a single round of rock, paper, and scissors
+
+function playRound(playerSelection, computerSelection) {
 
 }
-// function; plays a single round of Rock, Paper, Scissors
+const playSelection = prompt("Rock? Paper? or Scissors?", '');
+const computerSelection = computerPlay()
