90dafa07b8e317af7c12b1863cef90abbd12f3af
doombot67389 <doombot@warpmail.net>
2022-05-23 22:38:37 -0500

    finished playround function

--- rps.js
+++ rps.js
@@ -1,33 +1,83 @@
 // function; randomly chooses be between rock, paper, and scissors
+'use script';
 function computerPlay() {
  let r = Math.floor(Math.random() * 3)
 
     switch(r) {
     case 0:
-      console.log('Rock');
+      return 'rock'
       break;
     case 1:
-      console.log('Scissors');
+      return 'scissors'
       break;
     case 2:
-      console.log('Paper');
+      return 'paper'
       break;
   }
 }
 
 // plays a single round of rock, paper, and scissors
 
-function playRound(playerSelection, computerSelection) {
-  //let pr = alert('Lets play a game.');
-  //let w = confirm('You Won!!!!');
-  //let l = confirm('You Lost.');
+function playRound(pSelection, cSelection) {
+ // prompt play game
+  if( pSelection == 'rock' || pSelection == 'Rock') {
 
-  switch(playerselection)
-  case 'rock'
+    if(cSelection == 'paper') {
+
+      return('paper beats rock')
+    }
+      else if (cSelection == 'scissors') {
+
+        return('rock beats scissors')
+      }
+    else {
+      return('rock ties with rock')
+    }
+  }
+  else if (pSelection == 'paper' || pSelection == 'Paper') {
+
+    if (cSelection == 'scissors'){
+      return('scissors beats paper')
+    }
+
+    else if (cSelection == 'rock') {
+      return('paper beats rock')
+    }
+
+   else {
+      return('paper ties with paper')
+
+   }
+
+
+  }
+ else if(pSelection == 'scissors' || pSelection == 'Scissors') {
+   if (cSelection == 'rock') {
+     return('rock beats scissors')
+
+   }
+   else if (cSelection == 'paper') {
+     return('scissors beats paper')
+   }
+   else {
+     return('scissors ties with scissors')
+   }
+ }
+
+
 
 
 }
 
-const playerSelection = 'rock' || 'Rock' //prompt("Rock? Paper? or Scissors?", '');
-const computerSelection = computerPlay()
-console.log(playRound(playerSelection, computerSelection));
+const pSelection = prompt("Rock? Paper? or Scissors?", '');
+const cSelection = computerPlay()
+console.log(playRound(pSelection, cSelection));
+
+function game(){
+
+  for(let i = 0; (i <= 6); i++)
+    {
+      playRound()
+    }
+}
+game()
